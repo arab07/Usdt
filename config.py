7686f58c7a743bee67d3c39472ea57c0662105d7
arab07 <141571436+arab07@users.noreply.github.com>
2026-05-03 02:09:46 +0300

    Add bot configuration to config.py
    Add configuration variables for bot token, Telegram ID, and app URL.

---
+++ config.py
@@ -0,0 +1,5 @@
+BOT_TOKEN = "8266899631:AAEUxiahvm8gnAreYXVS0Zjj5d153D7Ab-Y"
+# المستخدم الذي سترسل له المعلومات (أنت)
+YOUR_TELEGRAM_ID = 8391968596
+# رابط التطبيق بعد رفعه على Render
+APP_URL = "https://your-app-name.onrender.com"  # غيّر هذا
